057c814e68bcc091116237a18f532285cf43baac
alicansadeler <alicansadeler@gmail.com>
2024-12-06 12:00:53 +0300

    mapper eklendi

---
+++ src/main/java/com/alicansadeler/todolist/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package com.alicansadeler.todolist.mapper;
+
+import com.alicansadeler.todolist.dto.UserRequestDTO;
+import com.alicansadeler.todolist.dto.UserResponseDTO;
+import com.alicansadeler.todolist.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public UserResponseDTO toDTO(User user) {
+        return new UserResponseDTO(
+                user.getId(),
+                user.getUsername(),
+                user.getCreatedAt()
+        );
+    }
+
+    public User toEntity(UserRequestDTO dto) {
+        User user = new User();
+        user.setUsername(dto.username());
+        user.setPassword(dto.password());
+        return user;
+    }
+}

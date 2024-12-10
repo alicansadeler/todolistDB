d6aac0b6554928f7efcbb4b70dc5331ad460f502
alicansadeler <alicansadeler@gmail.com>
2024-12-10 16:44:36 +0300

    Auth paths eklendi

--- src/main/java/com/alicansadeler/todolist/paths/ApiUrls.java
+++ src/main/java/com/alicansadeler/todolist/paths/ApiUrls.java
@@ -1,5 +1,7 @@
 package com.alicansadeler.todolist.paths;
 
+import com.alicansadeler.todolist.service.AuthService;
+
 public final class ApiUrls {
     private ApiUrls() {}
 
@@ -11,4 +13,9 @@ public final class ApiUrls {
     public static final String ID = "/{id}";
     public static final String BY_USER_ID = "/user/{userId}";
     public static final String BY_STATUS = "/status/{status}";
+
+    public static final String AUTH = BASE + "/auth";
+    public static final String LOGIN = AUTH + "/login";
+    public static final String REGISTER = AUTH + "/register";
+
 }

d9aee53ff2762f9907fc642b70424a506465b3f4
alicansadeler <alicansadeler@gmail.com>
2024-12-15 21:54:28 +0300

    OpenAPIConfig eklendi

---
+++ src/main/java/com/alicansadeler/todolist/config/OpenAPIConfig.java
@@ -0,0 +1,36 @@
+package com.alicansadeler.todolist.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("TodoList API")
+                        .description("Todo List API Documentation")
+                        .version("v1")
+                        .contact(new Contact()
+                                .name("Ali Can Sadeler")
+                                .email("alicansadeler@gmail.com"))
+                        .license(new License().name("License of API")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
